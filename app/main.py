c57160690067b6914c836982b6ae7253ac7b2cb4
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2023-09-05 14:04:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -104,7 +104,7 @@ def process_and_display_results(extract_paths):
 
 
 # Titre de l'application
-st.title("Identification de genres musicaux")
+st.title("Identification de ")
 
 upload_folder = "audio_files"
 os.makedirs(upload_folder, exist_ok=True)
